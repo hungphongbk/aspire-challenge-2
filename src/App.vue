28fd3f5bb90cb28c0dff56ba777adcbf5757a5fd
hungphongbk <programmingd32@gmail.com>
2021-03-04 19:30:29 +0700

    test desktop UI

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,97 @@
 <template>
   <component :is="Layout">
-    hihi
+    <div :class="$style.Container">
+      <h2 :class="$style.Title">Debit Card</h2>
+      <div :class="$style.Panel">
+        <div class="row">
+          <div class="col-12 col-md-6">
+            <span>Available Balance</span>
+            <div class="row justify-between items-center">
+              <div><span :class="$style.Badge">S$</span><span>3,000</span></div>
+              <div :class="$style.TopUp">
+                <top-up-icon style="vertical-align: middle"></top-up-icon>
+                &nbsp;Top up
+              </div>
+            </div>
+            <credit-card style="margin-top:40px;"></credit-card>
+          </div>
+          <div class="col-12 col-md-6"></div>
+        </div>
+      </div>
+    </div>
   </component>
 </template>
 
 <script>
 import DesktopLayout from "@/layout/DesktopLayout";
 import MobileLayout from "@/layout/MobileLayout";
+import TopUpIcon from "@/assets/TopUpIcon";
+import CreditCard from "@/components/CreditCard";
 
 export default {
   name: 'LayoutDefault',
-  computed:{
-    Layout(){
-      return this.$device.mobile?MobileLayout:DesktopLayout
+  components:{
+    TopUpIcon,
+    CreditCard
+  },
+  computed: {
+    Layout() {
+      return this.$device.mobile ? MobileLayout : DesktopLayout
     }
   },
 }
 </script>
 
-<style>
+<style lang="scss" module>
+@import "styles/mixins";
+
+.Container {
+  @include for-desktop-up {
+    padding-left: 60px;
+    padding-top: 60px;
+  }
+}
+
+h2.Title {
+  display: inline-block;
+  margin: 0;
+  padding: 0;
+  line-height: 1;
+
+
+  @include for-desktop-up {
+    font-size: 24px;
+    font-weight: 700;
+  }
+}
+
+.Panel{
+  @include for-desktop-up{
+    border:1px solid #FCFCFC;
+    border-radius:8px;
+    box-shadow:0 3px 16px #00000014;
+    padding:40px;
+    margin-top:40px;
+  }
+}
+
+.Badge{
+  border-radius:4px;
+  padding:3px 12px;
+  background-color:#01D167;
+  color:white;
+  font-size:13px;
+  font-weight:700;
+  &+span{
+    font-size:26px;
+    font-weight:bold;
+    margin-left: 12px;
+    vertical-align: sub;
+  }
+}
+.TopUp{
+  font-size: 14px;
+  color:#23CEFD;
+  font-weight: 600;
+}
 </style>
---
+++ src/components/CreditCard.vue
@@ -0,0 +1,52 @@
+<template>
+  <div :class="$style.Container">
+    <div :class="$style.Eye">
+      <removed-eye-icon></removed-eye-icon>
+    </div>
+    <div class="row justify-end">
+      <aspire-logo-white></aspire-logo-white>
+    </div>
+    <div :class="$style.Name">Mark Henry</div>
+  </div>
+</template>
+<script>
+import RemovedEyeIcon from "@/assets/RemovedEyeIcon";
+import AspireLogoWhite from "@/assets/AspireLogoWhite";
+
+export default {
+  name: "CreditCard",
+  components: {
+    RemovedEyeIcon,
+    AspireLogoWhite
+  }
+}
+</script>
+<style module lang="scss">
+.Container {
+  background-color: #01D167;
+  padding: 24px;
+  border-radius: 12px;
+  box-shadow: 0 2px 6px #00000014;
+  position: relative;
+  color:white;
+}
+
+.Eye {
+  composes: row justify-center items-center from global;
+  position: absolute;
+  top: -2px;
+  left: -2px;
+  width: 32px;
+  height: 32px;
+  background: white;
+  border-radius: 50%;
+  box-shadow: 0 2px 6px #072a441f;
+  cursor: pointer;
+}
+.Name{
+  font-size:22px;
+  font-weight:600;
+  margin-top:24px;
+  margin-bottom:24px;
+}
+</style>
