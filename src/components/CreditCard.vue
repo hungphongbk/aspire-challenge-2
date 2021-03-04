6da0f30bfbbb737f82cf60b67db77fe6f93183c3
hungphongbk <programmingd32@gmail.com>
2021-03-04 19:54:06 +0700

    test desktop UI

--- src/components/CreditCard.vue
+++ src/components/CreditCard.vue
@@ -1,23 +1,43 @@
 <template>
-  <div :class="$style.Container">
-    <div :class="$style.Eye">
-      <removed-eye-icon></removed-eye-icon>
+  <q-responsive :ratio="366/220">
+    <div  :class="$style.Container">
+      <div :class="$style.Eye">
+        <removed-eye-icon></removed-eye-icon>
+      </div>
+      <div class="row justify-end">
+        <aspire-logo-white></aspire-logo-white>
+      </div>
+      <div :class="$style.Name">Mark Henry</div>
+      <div class="row items-center">
+        <div
+            v-for="item in 4"
+            :key="item"
+            :class="$style.Dots">
+          <span
+              v-for="item in 4"
+              :key="item" />
+        </div>
+        <span style="font-weight: 600;letter-spacing: 6px;">2020</span>
+      </div>
+      <div :class="$style.Cvv">
+        <span>Thru: 12/20</span>
+        <span style="margin-left: 32px">CVV: <span style="font-size: 24px;line-height: 0;vertical-align: middle">* * *</span></span>
+      </div>
+      <div :class="$style.VisaLogo"><visa-logo></visa-logo></div>
     </div>
-    <div class="row justify-end">
-      <aspire-logo-white></aspire-logo-white>
-    </div>
-    <div :class="$style.Name">Mark Henry</div>
-  </div>
+  </q-responsive>
 </template>
 <script>
 import RemovedEyeIcon from "@/assets/RemovedEyeIcon";
 import AspireLogoWhite from "@/assets/AspireLogoWhite";
+import VisaLogo from "@/assets/VisaLogo";
 
 export default {
   name: "CreditCard",
   components: {
     RemovedEyeIcon,
-    AspireLogoWhite
+    AspireLogoWhite,
+    VisaLogo
   }
 }
 </script>
@@ -48,5 +68,28 @@ export default {
   font-weight:600;
   margin-top:24px;
   margin-bottom:24px;
+}
+.Dots{
+  composes:row justify-between items-center from global;
+  margin-right: 18px;
+  >span{
+    width: 8px;
+    height: 8px;
+    border-radius: 50%;
+    display: block;
+    background-color: white;
+    margin-right: 6px;
+  }
+}
+.Cvv{
+  margin-top:15px;
+font-size:13px;
+  font-weight:600;
+  letter-spacing: 1.56px;
+}
+.VisaLogo{
+  position: absolute;
+  bottom:24px;
+  right:24px;
 }
 </style>
